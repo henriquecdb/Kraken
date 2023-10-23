50e02342b61196e9fe2b83352c8124abb25011cb
RafaelColi <rafacolialves@gmail.com>
2023-10-23 17:28:54 -0300

    Add cadastro no banco de dados

---
+++ php/cadastrar.php
@@ -0,0 +1,34 @@
+<?php
+
+$nome = $_POST["nameCadastro"];
+$email = $_POST["emailCadastro"];
+$senha = $_POST["passwdCadastro"];
+
+$servername = "108.179.253.195";
+$username = "proje500_ralves";
+$password = "5cYPggNpnK36";
+$dbname = "proje500_G42023";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO Aluno VALUES ('$nome', '$email', 16/06/2001, 105, 0, '$senha')";
+$result = $conn->query($sql);
+$conn->close();
+
+
+if ($result === TRUE) {
+    header("Location: ../pages/login.php");
+    exit();
+    //echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+?>
