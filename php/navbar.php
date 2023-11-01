7be83aa7a4eaa17dc6d5c5435ada02dc46174499
henriquecdb <henriquecamposj@gmail.com>
2023-11-01 17:23:17 -0300

    Add search to navbar

--- php/navbar.php
+++ php/navbar.php
@@ -24,8 +24,12 @@
               <a class="nav-link" href="contest.php">Contests</a>
             </li>
           </ul>
+          <form class="d-flex" role="search" action="/search.php">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
           <form class="d-flex" role="search" action="login.php">
-            <button class="btn btn-outline-success" type="submit">Login</button>
+            <!-- <button class="btn btn-outline-success" type="submit">Login</button> -->
           </form>
         </div>
       </div>
