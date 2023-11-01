f4cebbb15f4ffeb4deafb5da528687659404beb9
henriquecdb <henriquecamposj@gmail.com>
2023-11-01 17:19:21 -0300

    Finished page to retrieve password

--- pages/forget.php
+++ pages/forget.php
@@ -4,32 +4,44 @@
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Kraken - Forget</title>
+      <title>Kraken</title>
+      <link rel="stylesheet" href="../styles/style.css" />
       <link
-         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
          rel="stylesheet"
-         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-         crossorigin="anonymous"
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
          />
    </head>
    <body>
-      <div class="container">
-         <div class="row justify-content-center">
-            <div class="col-lg-5">
-               <div class="card">
-                  <div class="card-header">Redefinir Senha</div>
-                  <div class="card-body">
-                     <form action=".." method="post">
-                        <div class="form-group">
-                           <label for="email">Endereço de Email</label>
-                           <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Redefinir Senha</button>
-                     </form>
-                  </div>
+      <h2>Bem-vindo ao KRAKEN</h2>
+      <div class="container" id="container">
+         <div class="form-container sign-in-container">
+            <form action="../php/logar.php" method="post">
+               <h2 style="margin-bottom: 5px !important">
+               Recupere sua senha</h1>
+               <span>Use seu email</span>
+               <input type="email" placeholder="Email" name="emailLogin" id="emailLogin" />
+               <input type="password" placeholder="Password" name="passwdLogin" id="passwdLogin" />
+               <button type="submit" >Enviar</button>
+            </form>
+         </div>
+         <div class="overlay-container">
+            <div class="overlay">
+               <div class="overlay-panel overlay-left">
+                  <h1>Bem-vindo!</h1>
+                  <p>
+                     Para se manter conectado, entre com suas informações
+                  </p>
+                  <button class="ghost" id="signIn">Entrar</button>
+               </div>
+               <div class="overlay-panel overlay-right">
+                  <h1>Olá, Amigo!</h1>
+                  <p>Recupere suas informações e continue sua jornada</p>
                </div>
             </div>
          </div>
       </div>
+      <footer>
+         <p>Created by <a href="https://github.com/henriquecdb/Kraken">Kraken</a></p>
+      </footer>
    </body>
 </html>
