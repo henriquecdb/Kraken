ed5968af7d19dfa078f302a4efb4cdf267f90026
RafaelColi <rafacolialves@gmail.com>
2023-11-06 16:53:49 -0300

    codigos php e hmtl separados

--- pages/problem.php
+++ pages/problem.php
@@ -1,65 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Kraken - Page</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossorigin="anonymous"
-    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <style>
-      .limites li {
-        display: inline-block;
-        list-style-type: none;
-        margin-right: 10px;
-      }
-      .limites span {
-        border-bottom: 1px solid;
-      }
+        .limites li {
+            display: inline-block;
+            list-style-type: none;
+            margin-right: 10px;
+        }
+
+        .limites span {
+            border-bottom: 1px solid;
+        }
+
+        pre {
+            white-space: pre-wrap;
+            background-color: #f8f9fa;
+            padding: 10px;
+            border: 1px solid #dee2e6;
+            border-radius: 4px;
+        }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- Navbar -->
     <?php include "../php/navbar.php" ?>
     
-    <!-- Main Section -->
-    <div class="container mt-5">
-      <h1 class="mb-3">0001 - Soma Fácil</h2>
-      <hr>
-      <ul class="limites">
-        <li><span>Time Limit:</span> 1.00s</li>
-        <li><span>Memory Limit:</span> 512 MB</li>
-      </ul>
+    <?php include "../php/problem.php" ?>
 
-      <p>Calcular a soma de dois números inteiros A e B.</p>
-
-      <h5>Input</h5>
-      <p>O input consiste em dois números inteiros separados por um espaço.</p>
-      <h5>Output</h5>
-      <p>O output deve ser a soma dos dois números.</p>
-      <h5>Example</h5>
-      <p>
-        Input:<br />
-        2 3
-      </p>
-      <p>
-        Output:<br />
-        5
-      </p>
-      <button type="button" class="btn btn-primary">Submeter Solução</button>
-    </div>
-
-    <!-- footer -->
     <?php include "../php/footer.php" ?>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- php/problem.php
+++ php/problem.php
@@ -3,6 +3,7 @@ $servername = "108.179.253.195";
 $username = "proje500_ralves";
 $password = "5cYPggNpnK36";
 $dbname = "proje500_G42023";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -21,80 +22,42 @@ if (isset($_GET['id']) && $_GET['id'] !== null) {
     if ($result->num_rows == 1) {
         $questaoDetalhes = $result->fetch_assoc();
 ?>
-        <!DOCTYPE html>
-        <html lang="pt-br">
+<head>
+<title><?php echo $questaoDetalhes['Titulo']; ?> - Kraken - Page</title>
+    </head>
 
-        <head>
-            <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title><?php echo $questaoDetalhes['Titulo']; ?> - Kraken - Page</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-            <style>
-                .limites li {
-                    display: inline-block;
-                    list-style-type: none;
-                    margin-right: 10px;
-                }
+    <!-- Main Section -->
+<div class="container mt-5" id="questao-<?php echo $idQuestao; ?>">
+    <div class="container mt-5">
+        <h1 class="mb-3"><?php echo $questaoDetalhes['Titulo']; ?></h1>
+        <hr>
+        <ul class="limites">
+            <li><span>Time Limit:</span> 1.00s</li>
+            <li><span>Memory Limit:</span> 512 MB</li>
+        </ul>
 
-                .limites span {
-                    border-bottom: 1px solid;
-                }
+        <p><?php echo $questaoDetalhes['Descricao']; ?></p>
 
-                pre {
-                    white-space: pre-wrap;
-                    background-color: #f8f9fa;
-                    padding: 10px;
-                    border: 1px solid #dee2e6;
-                    border-radius: 4px;
-                }
-            </style>
-        </head>
+        <h5>Input</h5>
+        <pre><?php echo $questaoDetalhes['Descricao_Input']; ?></pre>
 
-        <body>
-            <!-- Navbar -->
-            <?php include "../php/navbar.php" ?>
-
-            <!-- Main Section -->
-            <div class="container mt-5" id="questao-<?php echo $idQuestao; ?>">
-            <div class="container mt-5">
-                <h1 class="mb-3"><?php echo $questaoDetalhes['Titulo']; ?></h1>
-                <hr>
-                <ul class="limites">
-                    <li><span>Time Limit:</span> 1.00s</li>
-                    <li><span>Memory Limit:</span> 512 MB</li>
-                </ul>
-
-                <p><?php echo $questaoDetalhes['Descricao']; ?></p>
-
-                <h5>Input</h5>
-                <pre><?php echo $questaoDetalhes['Descricao_Input']; ?></pre>
-
-                <h5>Output</h5>
-                <pre><?php echo $questaoDetalhes['Descricao_Output']; ?></pre>
-                <div class="mb-3">
-                    <button type="button" class="btn btn-primary mr-2">Submeter Solução</button>
-                    <form action="update_question.php" method="post" class="d-inline">
-                    <a href="update_question.php?id=<?php echo $idQuestao; ?>" class="btn btn-warning">Editar Questão</a>
-                    </form>
-                    <form action="../php/delete_question.php" method="post" class="d-inline">
-                        <input type="hidden" name="id" value="<?php echo $idQuestao; ?>">
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja deletar esta questão?')">Deletar Questão</button>
-                    </form>
-                    <form class="d-inline">
-                    <a href="javascript:history.back()" class="btn btn-secondary">Voltar</a>
-                    </form>
-                </div>
-            </div>
-            </div>
-
-            <!-- Footer -->
-            <?php include "../php/footer.php" ?>
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        </body>
-
-        </html>
+        <h5>Output</h5>
+        <pre><?php echo $questaoDetalhes['Descricao_Output']; ?></pre>
+        <div class="mb-3">
+            <button type="button" class="btn btn-primary mr-2">Submeter Solução</button>
+            <form action="update_question.php" method="post" class="d-inline">
+            <a href="update_question.php?id=<?php echo $idQuestao; ?>" class="btn btn-warning">Editar Questão</a>
+            </form>
+            <form action="../php/delete_question.php" method="post" class="d-inline">
+                <input type="hidden" name="id" value="<?php echo $idQuestao; ?>">
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja deletar esta questão?')">Deletar Questão</button>
+            </form>
+            <form class="d-inline">
+            <a href="javascript:history.back()" class="btn btn-secondary">Voltar</a>
+            </form>
+        </div>
+    </div>
+</div>
 <?php
     } else {
         echo "Questão não encontrada.";
