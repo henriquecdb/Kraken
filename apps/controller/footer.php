e259b346f04bc52ecf6b9fcebb896313a782c728
henriquecdb <henriquecamposj@gmail.com>
2023-11-16 10:09:52 -0300

    Testing new footer dirs

--- apps/controller/footer.php
+++ apps/controller/footer.php
@@ -2,19 +2,19 @@
     <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item">
-        <a href="../views/page.php" class="nav-link px-2 text-body-secondary">Home</a>
+        <a href="../apps/views/page.php" class="nav-link px-2 text-body-secondary">Home</a>
         </li>
         <li class="nav-item">
-        <a href="../views/problemset.php" class="nav-link px-2 text-body-secondary">Problemset</a>
+        <a href="../apps/views/problemset.php" class="nav-link px-2 text-body-secondary">Problemset</a>
         </li>
         <li class="nav-item">
-        <a href="../views/support.php" class="nav-link px-2 text-body-secondary">Support</a>
+        <a href="../apps/views/support.php" class="nav-link px-2 text-body-secondary">Support</a>
         </li>
         <li class="nav-item">
-        <a href="../views/faq.php" class="nav-link px-2 text-body-secondary">FAQs</a>
+        <a href="../apps/views/faq.php" class="nav-link px-2 text-body-secondary">FAQs</a>
         </li>
         <li class="nav-item">
-        <a href="../views/about.php" class="nav-link px-2 text-body-secondary">About</a>
+        <a href="../apps/views/about.php" class="nav-link px-2 text-body-secondary">About</a>
         </li>
     </ul>
     <p class="text-center text-body-secondary">© 2023 Kraken, Inc</p>
