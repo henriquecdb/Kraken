25d6a86d36d16b4e94356fcfb46efcee9a20ab27
RafaelColi <rafacolialves@gmail.com>
2023-11-17 10:15:15 -0300

    Arquivo de conexão com o banco

--- php/sql_connection.php
+++ php/sql_connection.php
@@ -1,28 +1,21 @@
 <?php
-$servername = "108.179.253.195";
-$username = "proje500_ralves";
-$password = "5cYPggNpnK36";
-$dbname = "proje500_G42023";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+function connect_db() {
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+  $servername = "108.179.253.195";
+  $username = "proje500_ralves";
+  $password = "5cYPggNpnK36";
+  $dbname = "proje500_G42023";
 
-$sql = "SELECT * FROM Aluno";
-$result = $conn->query($sql);
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "Nome: " . $row["Nome_Aluno"]. " - Email: " . $row["Email"]. " - Data de Nascimento: " . $row["Data_Nascimento"]. " - ID Aluno: " . $row["ID_Aluno"] . " - Ranking: " . $row["Ranking"] . "<br>";
-    }
-  } else {
-    echo "0 results";
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . mysqli_connect_error());
   }
 
-$conn->close();
+  return $conn;
+}
+
 ?>
